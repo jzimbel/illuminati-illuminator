ef5659b593f17c245c0ce4fa583f302bd0c4f888
jzimbel <jzimbel@gmail.com>
2019-05-15 00:20:27 -0400

    More cleanup and comments

--- Source/content_script.js
+++ Source/content_script.js
@@ -13,30 +13,13 @@ const theories = [
   'RuPaul is the one true god'
 ];
 
-const getTheory = () => {
-  return randomChoice(theories);
-};
-
 const randomChoice = options =>
   options[Math.floor(Math.random() * options.length)];
 
-const getChars = str => [...str.toLowerCase()].filter(char => /\w/.test(char));
-
-const getCharsForReplace = str => [...str];
-
-// Returns true if a node should be checked for text to change
-const isCandidate = node =>
-  !(
-    node.isContentEditable || // DraftJS and many others
-    (node.parentNode && node.parentNode.isContentEditable) || // Special case for Gmail
-    (node.tagName && // Some catch-alls
-      (node.tagName.toLowerCase() == 'textarea' ||
-        node.tagName.toLowerCase() == 'input'))
-  );
+const getTheoryCharacters = str =>
+  [...randomChoice(theories).toLowerCase()].filter(char => /\w/.test(char));
 
 const walk = (characters, rootNode) => {
-  let toFindIndex = 0;
-
   // Find all the text nodes in rootNode
   const walker = document.createTreeWalker(
     rootNode,
@@ -44,82 +27,87 @@ const walk = (characters, rootNode) => {
     null,
     false
   );
-  let node;
 
+  let node;
+  let toFindIndex = 0;
   const updaters = [];
-  // Modify each text node's value
-  while ((node = walker.nextNode())) {
-    const {offset, updater} = handleText(node, characters.slice(toFindIndex));
+  while (toFindIndex < characters.length && (node = walker.nextNode())) {
+    const {offset, updater} = getNodeUpdater(
+      node,
+      characters.slice(toFindIndex)
+    );
     toFindIndex += offset;
-    updaters.push(updater);
-    if (toFindIndex === characters.length) {
-      break;
-    }
+    updater && updaters.push(updater);
   }
   if (toFindIndex === characters.length) {
-    updaters
-      .filter(updater => updater !== null)
-      .forEach(updater => {
-        updater();
-      });
+    updaters.forEach(updater => {
+      updater();
+    });
   }
 };
 
-const getFontResize = () => `${100 + Math.ceil(Math.random() * 100)}%`;
+const getFontSize = () => `${100 + Math.ceil(Math.random() * 100)}%`;
 
 const getBackgroundColor = () =>
   randomChoice(['#ff69b4', '#56ff00', '#6a7e25', '#d6c601']);
 
-const getFontFamily = () => randomChoice(['sans-serif', 'serif', 'cursive', 'monospace']);
+const getFontFamily = () =>
+  randomChoice(['sans-serif', 'serif', 'cursive', 'monospace']);
 
 const getHighlightedElement = str => {
   const highlightedElement = document.createElement('strong');
   highlightedElement.innerText = str;
   highlightedElement.style.background = getBackgroundColor();
-  highlightedElement.style.fontSize = getFontResize();
+  highlightedElement.style.fontSize = getFontSize();
   highlightedElement.style.fontFamily = getFontFamily();
   return highlightedElement;
 };
 
-const handleText = (textNode, characters) => {
+// Reads through a text node and finds characters to highlight.
+// Returns an object of the shape: {
+//   offset: number of characters found to highlight,
+//   updater: false if no interesting characters found in this node,
+//            or a function that edits the DOM to highlight characters
+// }
+const getNodeUpdater = (textNode, characters) => {
   let offset = 0;
-  const newNodeParts = getCharsForReplace(textNode.nodeValue).reduce(
-    (parts, char) => {
-      if (char.toLowerCase() === characters[offset]) {
-        offset++;
-        return parts.concat(getHighlightedElement(char));
-      } else if (
-        parts.length > 0 &&
-        typeof parts[parts.length - 1] === 'string'
-      ) {
-        parts[parts.length - 1] = parts[parts.length - 1].concat(char);
-        return parts;
-      } else {
-        return parts.concat(char);
-      }
-    },
-    []
-  );
+  // an array of strings and Element objects that will replace this node.
+  // strings contain characters that are left alone,
+  // Elements contain characters that are highlighted as part of the hidden message
+  const newNodeParts = [...textNode.nodeValue].reduce((parts, char) => {
+    if (char.toLowerCase() === characters[offset]) {
+      offset++;
+      return parts.concat(getHighlightedElement(char));
+    } else if (
+      parts.length > 0 &&
+      typeof parts[parts.length - 1] === 'string'
+    ) {
+      parts[parts.length - 1] = parts[parts.length - 1].concat(char);
+      return parts;
+    } else {
+      return parts.concat(char);
+    }
+  }, []);
   return {
     offset,
-    updater: newNodeParts.some(part => typeof part === 'object')
-      ? () => {
-          textNode.replaceWith(...newNodeParts);
-        }
-      : null
+    updater:
+      newNodeParts.some(part => part instanceof Element) &&
+      (() => {
+        textNode.replaceWith(...newNodeParts);
+      })
   };
 };
 
 // Walk the doc (document) body and highlight characters in the conspiracy theory
 const highlightSuspiciousCharacters = doc => {
-  // only operate on pages that have a <main> element so that we have more of a chance of all of the
+  // only operate on pages that have a <main> element so that we have a better
+  // chance that all of the highlighted characters actually appear in visible
+  // elements within the main content
   const mainElements = doc.body.getElementsByTagName('main');
   const hasMain = mainElements.length > 0;
   if (hasMain) {
     const main = mainElements[0];
-    // get the characters of a random conspiracy theory
-    const characters = getChars(getTheory());
-
+    const characters = getTheoryCharacters();
     walk(characters, main);
   }
 };
